257ee7258a4d5f032680b4989a027efb7fac6b92
LMorel77 <lmorel77@aol.com>
2018-04-03 14:27:18 -0400

    Updates

--- zomatoapp.js
+++ zomatoapp.js
@@ -17,10 +17,9 @@ $(document).ready(function () {
         $("#hits").empty();
 
         city = $("#city-input").val().trim().replace(/\s+/g, ''); // Remove ALL spaces
-        cuisine = $("#cuisine-input").val().trim().replace(/\s+/g, ''); // remove ALL spaces
 
-        console.log("city: " + city);
-        console.log("cuisine: " + cuisine);
+        console.log("[In api-call] city: " + city);
+        console.log("[In api-call] cuisine: " + cuisine);
 
         $.ajax({
             type: "GET",
@@ -37,6 +36,10 @@ $(document).ready(function () {
 
                 console.log("City apiData: ", data);
 
+                var message = $("<span>");
+                message.text("Select Your City:");
+                $("#head").html(message);
+
                 if (data.location_suggestions.length === 0) {
 
                     $("#hits").html("<p>Unable to locate specified city name. Please check your spelling and try again.</p>");
@@ -47,14 +50,20 @@ $(document).ready(function () {
 
                     apiData = data;
 
-                    for (let i = 0; i < data.location_suggestions.length; i++) {
+                    console.log("[api-Call] option: ", $("#cuisine-input option:selected").val());
 
-                        var button = $("<button>");
-                        button.text(data.location_suggestions[i].name);
-                        button.attr('id', i);
-                        button.attr('class', 'buttons cities');
-                        $("#hits").append(button);
+                    if ($("#cuisine-input option:selected").val() === "null") {
 
+                        for (let i = 0; i < data.location_suggestions.length; i++) {
+
+                            var button = $("<button>");
+                            button.text(data.location_suggestions[i].name);
+                            button.val(data.location_suggestions[i].name);
+                            button.attr('id', i);
+                            button.attr('class', 'buttons cities');
+                            $("#hits").append(button);
+
+                        }
                     }
 
                 }
@@ -66,11 +75,19 @@ $(document).ready(function () {
 
         event.preventDefault();
 
+        $("#head").empty();
+        $("#hits").empty();
+
+        $("#city-input").val($(this).val());
+        console.log("City button 'this': ", $(this).val());
+
+        $("#cuisine-input").css("display", "inline");
+
         var index = $(this).attr('id');
         cityId = apiData.location_suggestions[index].id;
 
-        console.log("index: " + index);
-        console.log("cityId: ", cityId);
+        console.log("[In cities] index: " + index);
+        console.log("[In cities] cityId: ", cityId);
 
         $.ajax({
             type: "GET",
@@ -95,67 +112,116 @@ $(document).ready(function () {
                 }
                 else {
 
-                    var userCuisine = cuisine.toLowerCase();
-                    var apiCuisine;
-
                     for (let i = 0; i < data.cuisines.length; i++) {
 
-                        apiCuisine = (data.cuisines[i].cuisine.cuisine_name).toLowerCase();
-
-                        if (apiCuisine === userCuisine) {
-
-                            cuisineId = (data.cuisines[i].cuisine.cuisine_id).toString();
-                            i = data.cuisines.length;
-                            console.log("cuisineId: ", cuisineId);
-
-                        }
+                        var option = $("<option>");
+                        option.val(data.cuisines[i].cuisine.cuisine_name);
+                        option.text(data.cuisines[i].cuisine.cuisine_name);
+                        option.attr("data-cuisineId", data.cuisines[i].cuisine.cuisine_id);
+                        $("#cuisine-input").append(option);
 
                     }
 
-                    $.ajax({
-                        type: "GET",
-                        headers: {
-                            'X-Zomato-API-Key': apiKey //only allowed non-standard header
-                        },
-                        url: searchURL,
-                        dataType: 'json',
-                        data: {
-                            cuisines: cuisineId,
-                            entity_id: cityId,
-                            entity_type: 'city'
-                        },
-                        processData: true, // Converts data to query string
-                        success: function (data) {
+                }
+            }
+        });
+    });
 
-                            console.log("Search apiData: ", data);
+    $("#cuisine-input").on("change", function (event) {
 
-                            $("#hits").empty();
+        event.preventDefault();
+        cuisine = $("#cuisine-input option:selected").val();
+        city = $("#city-input").val();
 
-                            if (data.restaurants.length === 0) {
-                                $("#hits").html("<p>Unable to locate restaurants serving " + cuisine + ". Please check your spelling and try again, or try a different cuisine and/or city.</p>");
-                                return;
-                            }
-                            else {
+        var message = $("<span>");
+        message.text(cuisine + " Restaurants in " + city);
+        $("#head").html(message);
 
-                                for (let i = 0; i < data.restaurants.length; i++) {
+        cuisineId = $("#cuisine-input option:selected").attr("data-cuisineId");
+        console.log("[In Select handler] cuisineId: ", cuisineId);
 
-                                    var anchor = $("<a>");
-                                    var button = $("<button>");
-                                    var url = data.restaurants[i].restaurant.url;
+        $.ajax({
+            type: "GET",
+            headers: {
+                'X-Zomato-API-Key': apiKey //only allowed non-standard header
+            },
+            url: searchURL,
+            dataType: 'json',
+            data: {
+                cuisines: cuisineId,
+                entity_id: cityId,
+                entity_type: 'city'
+            },
+            processData: true, // Converts data to query string
+            success: function (data) {
 
-                                    anchor.attr("href", url).attr("target", "_blank").attr("alt", "Venue Option");
-                                    anchor.append(button);
-                                    button.text(data.restaurants[i].restaurant.name);
-                                    button.attr('id', i).attr('class', 'buttons venues');
+                console.log("Search apiData: ", data);
 
-                                    $("#hits").append(anchor);
+                $("#hits").empty();
 
-                                }
+                if (data.restaurants.length === 0) {
+                    $("#hits").html("<p>Unable to locate restaurants serving " + cuisine + ". Please check your spelling and try again, or try a different cuisine and/or city.</p>");
+                    return;
+                }
+                else if ($("#cuisine-input option:selected").val() === "null") {
+                    return;
+                }
+                else {
 
-                            }
+                    // Display Restaurant Data
+                    // Venue Info: Name, City, Address, <hr>, cuisines, cost, zomato url
 
+                    for (let i = 0; i < data.restaurants.length; i++) {
+
+                        var anchor = $("<a>");
+                        var mainDiv = $("<div>");
+                        var p = $("<p>");
+                        var priceRange = data.restaurants[i].restaurant.price_range;
+                        var span = $("<span>");
+                        var subDiv1 = $("<div>");
+                        var subDiv2 = $("<div>");
+                        var subDiv3 = $("<div>");
+
+                        subDiv1.append(data.restaurants[i].restaurant.name);
+                        subDiv1.addClass("venueName");
+                        subDiv2.append(data.restaurants[i].restaurant.location.city);
+                        subDiv2.addClass("venueCity");
+                        subDiv3.append(data.restaurants[i].restaurant.location.address + "<hr>");
+                        p.append("Cuisines: " + data.restaurants[i].restaurant.cuisines + "<br>");
+                        for (var cost = 0; cost < priceRange; cost++) {
+                            span.append("$");
                         }
-                    });
+                        p.append("Cost: ", span);
+                        anchor.attr("href", data.restaurants[i].restaurant.url);
+                        anchor.attr("target", "_blank");
+                        anchor.text("Visit Zomato Restaurant Page");
+                        p.append("<br>");
+                        p.append(anchor);
+                        subDiv3.append(p);
+                        mainDiv.append(subDiv1);
+                        mainDiv.append(subDiv2);
+                        mainDiv.append(subDiv3);
+                        mainDiv.addClass("venues");
+
+                        $("#hits").append(mainDiv);
+
+                    }
+
+                    // Restaurant Buttons
+                    // for (let i = 0; i < data.restaurants.length; i++) {
+
+                    //     var anchor = $("<a>");
+                    //     var button = $("<button>");
+                    //     var url = data.restaurants[i].restaurant.url;
+
+                    //     anchor.attr("href", url).attr("target", "_blank").attr("alt", "Venue Option");
+                    //     anchor.append(button);
+                    //     button.text(data.restaurants[i].restaurant.name);
+                    //     button.attr('id', i).attr('class', 'buttons venues');
+
+                    //     $("#hits").append(anchor);
+
+                    // }
                 }
             }
         });
